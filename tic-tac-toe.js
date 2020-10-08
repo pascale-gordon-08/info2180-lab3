11d72911bd89e97e2b9ac0e4d4d2d145ddb97e49
Pascale Gordon <pascale_gordon@hotmail.com>
2020-10-08 18:31:07 -0500

    Added class name & onload

---
+++ tic-tac-toe.js
@@ -0,0 +1,19 @@
+"use strict";
+
+window.onload = function(){
+var bodyContent =document.getElementsByTagName("body");
+var childElements= document.body.children;
+var contents = document.getElementById("board").children;
+ for(var i=0; i<contents.length; i++){
+     contents[i].className="square";
+ } 
+};
+
+
+
+
+
+
+
+
+
