ab7d7b205a1098a8041e4550bbb18089394df86c
Pascale Gordon <pascale_gordon@hotmail.com>
2020-10-09 22:02:43 -0500

    Added X & O, hover

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,13 +7,72 @@ var contents = document.getElementById("board").children;
  for(var i=0; i<contents.length; i++){
      contents[i].className="square";
  } 
-};
+
+
+ var plays = [];
+
+let choice = true;
+ for(var i=0; i<contents.length; i++){
+     contents[i].classList.add('square');
+     contents[i].addEventListener("click", function(){
+         //xBox(this);
+         if(choice){
+             xBox(this);
+             choice=!choice;
+         }else{
+             oBox(this);
+             choice=!choice;
+         }
+   
+     }); 
+ } 
+
+  
+ for(var i=0; i<contents.length; i++){
+     var site = contents[i];
+    site.addEventListener('mouseover', function(e) {
+        e.target.classList.add('hover');
+      });
+      
+      site.addEventListener('mouseout', function(e) {
+        e.target.classList.remove('hover');
+      });
+    
+   
+} 
+
+
+ function xBox(place){
+     place.classList.add('X');
+     place.textContent='X';
+ }
+
+ function oBox(place){
+    place.classList.add('O');
+    place.textContent='O';
+    
+}
+
+
+ 
+
+
+ 
+}
 
 
 
-
-
-
+/* var xBox = document.getElementById(".square.x");
+ var oBox = document.getElementById(".square.o");
+ xBox.className.add("square");
+ oBox.className.add("square");
+ 
+ function choice(){
+     square.addEventListener('click', e => {
+         e.preventDefault();
+         document.getElementById(".square.x").innerHTML = xBox;
+       }); 
+} */
 
 
 
